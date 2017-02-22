c0a697678e8a51b564c00ab14549398bfd61aba7
DALY <thibaultdaly@live.fr>
2017-02-22 18:37:27 +0100

    New !

--- New.py
+++ New.py
@@ -1,1 +1,9 @@
 # Python work friday 23/02/2017
+import pandas
+
+data_returns = pandas.read_pickle('Returns.pkl')
+print(data_returns)
+
+data_sectors = pandas.read_pickle("Sector.pkl")
+data_SPX = pandas.read_pickle("SPX.pkl")
+data_strategy = pandas.read_pickle("Strategy.pkl")
